789fd4886af25da038933e756f1c8057564350c3
Pedro Hrafn Martinez <pedro16@ru.is>
2018-09-25 18:09:15 +0000

    refactoring and comments

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -21,7 +21,33 @@ export default class HomeScreen extends React.Component {
     this.setState({
       isReady: true
     });
+    this.state.data.sort(this.compare);
   }
+
+  // To make the our data fit for SectionList we want to change it's structure.
+  // This function returns our data as an array of objects where each object has
+  // a key named data, and a key named key, where data are all the items where
+  // the first letter in first_name is the same as the letter in key
+  fitDataForSectionList = () => {
+    // first we make an object of lists where the key is the first
+    // letter of each first_name and the data for each key is a list
+    // where the first letter in first_name fits the key
+    let fitData = this.state.data.reduce((cc, x) => {
+      const key = x.name.first_name[0];
+      if (!cc[key]) {
+        cc[key] = [];
+      }
+      cc[key].push(x);
+      return cc;
+    }, {});
+    // we convert the object to an array of objects that fits SectionList
+    fitData = Object.keys(fitData).map(x => {
+      obj = { data: fitData[x], key: x };
+      return obj;
+    });
+    return fitData;
+  };
+
   compare = (a, b) => {
     // Generic sorting function used in render
     if (a.name.first_name < b.name.first_name) return -1;
@@ -68,26 +94,8 @@ export default class HomeScreen extends React.Component {
   };
 
   render() {
-    console.log("BLALBLAA");
-    const { data, isReady } = this.state;
-    data.sort(this.compare);
-    let data2 = data.map(x => {
-      return { ...x, key: x.name.first_name[0] };
-    });
-    data2 = data2.reduce((cc, x) => {
-      if (!cc[x.key]) {
-        cc[x.key] = [];
-      }
-      cc[x.key].push(x);
-      return cc;
-    }, {});
-    let newData = Object.keys(data2).map(x => {
-      obj = { data: data2[x], key: x };
-      return obj;
-    });
-    // ends
-    //console.log(newData);
-    //console.log("penids");
+    let newData = this.fitDataForSectionList();
+
     return (
       <View style={styles.container}>
         <SectionList
